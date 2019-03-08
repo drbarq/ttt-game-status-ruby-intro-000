f8347e3196a102ed396e59e686b80d6e495e44ae
Joe Tustin <j.tustin@gmail.com>
2019-03-08 23:08:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -83,6 +83,9 @@ def draw?(board)
     puts "Draw2!"
     return true
   end
+  if won?(board) == true && full?(board) == true
+    puts "False2"
+    return false
   if won?(board) == true && full?(board) == false
     puts "False2"
     return false
