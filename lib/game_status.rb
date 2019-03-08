1133e1a958a837a98d71676daf3258ab3b78c937
Joe Tustin <j.tustin@gmail.com>
2019-03-08 20:39:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,9 +30,9 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
 
-      if position_1 == " " && position_2 == " " && position_3 == " "
-        return false
-      end 
+  ##    if position_1 == " " && position_2 == " " && position_3 == " "
+  ##      return false
+  ##    end 
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         puts position_1
