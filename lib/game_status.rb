0ceff228d334f6ae0b10ddda2375f36043432dc3
Joe Tustin <j.tustin@gmail.com>
2019-03-08 19:55:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,8 @@ def won?(board)
       win_index_3 = position[2]
 
       position_1 = board[win_index_1]
-      position_2 = board[win_index_2] 
-      position_3 = board[win_index_3] 
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination # return the win_combination indexes that won.
