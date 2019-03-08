6de72eeb5fb9ca9f86505d94e7aa7fdad120db99
Joe Tustin <j.tustin@gmail.com>
2019-03-08 20:49:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,10 +19,7 @@ WIN_COMBINATIONS = [
 ## using || did not work to group the two statements
 
 def won?(board)
-
   WIN_COMBINATIONS.each do |combo|
-
-
       win_index_1 = combo[0]
       win_index_2 = combo[1]
       win_index_3 = combo[2]
@@ -37,8 +34,8 @@ def won?(board)
         puts position_3
         puts combo
         return combo
-
       end
+      
       if position_1 == "O"  && position_2 == "O" && position_3 == "O"
         puts position_1
         puts position_2
