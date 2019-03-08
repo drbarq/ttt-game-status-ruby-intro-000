8abc103152fedd2fbaa82f04421a3210e5eea7a2
Joe Tustin <j.tustin@gmail.com>
2019-03-08 20:09:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def won?(board)
       position_3 = board[win_index_3]
 
       if position_1 == "X" || position_1 == "O"  && position_2 == "X" || position_2 == "O" && position_3 == "X" || position_3 == "O"
-        return combo
+        return combo[]
       else
         false
       end
