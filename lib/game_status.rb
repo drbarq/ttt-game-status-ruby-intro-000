2a9e586e933d34a8db2e8cf9c8555bc0cce7e9c8
Joe Tustin <j.tustin@gmail.com>
2019-03-08 20:41:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,6 +40,7 @@ def won?(board)
         puts position_3
         puts combo
         return combo
+      end 
       if position_1 == "O"  && position_2 == "O" && position_3 == "O"
         puts position_1
         puts position_2
