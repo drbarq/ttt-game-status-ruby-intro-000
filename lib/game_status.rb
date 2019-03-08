05b5850f71a6abc43cbab6c79cce765968eef490
Joe Tustin <j.tustin@gmail.com>
2019-03-08 19:32:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
       position_2 = board[WIN_COMBINATIONS[1]]
       position_3 = board[WIN_COMBINATIONS[2]]
 
-      if board[0] == "X"
+      if position_1 == "X"
         puts "winner"
 
 
