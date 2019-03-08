e6c83fce37b97a4f8495bdb373df569a3eb2bd38
Joe Tustin <j.tustin@gmail.com>
2019-03-08 20:06:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ def won?(board)
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
 
-        return WIN_COMBINATIONS
+        return combo
       else
         false
       end
